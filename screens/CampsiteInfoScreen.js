7c8925246d88be961cdb2f79d954fe745b2d3b6b
an <anthedev@gmail.com>
2023-09-16 07:53:02 -0700

    Navigation Part 1

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,7 +1,10 @@
 import RenderCampsite from '../features/campsites/RenderCampsite';
 
-const CampsiteInfoScreen = (props) => {
-    return <RenderCampsite campsite={props.campsite} />;
+const CampsiteInfoScreen = ({ route }) => {
+    const { campsite } = route.params;
+
+    return <RenderCampsite campsite={campsite} />;
+
 };
 
 export default CampsiteInfoScreen;
