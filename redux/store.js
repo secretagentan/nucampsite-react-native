a8b2ef156206d5c3fb218182f592503e2990db14
an <anthedev@gmail.com>
2023-09-30 07:01:00 -0700

    [solution] Persist Redux Store

--- redux/store.js
+++ redux/store.js
@@ -1,17 +1,26 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 import { favoritesReducer } from '../features/favorites/favoritesSlice';
-import { persistStore, persistCombineReducers, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import {
+    persistStore,
+    persistCombineReducers,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const config = {
     key: 'root',
     storage: AsyncStorage,
     debug: true
-}
+};
 
 export const store = configureStore({
     reducer: persistCombineReducers(config, {
